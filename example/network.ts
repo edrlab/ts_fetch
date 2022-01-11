ff6500df23501a980fbf6e9e7c72b6cd99451eee
Pierre Leroux <panacpp@gmail.com>
2022-01-11 17:09:16 +0100

    rename test/network

--- example/network.ts
+++ example/network.ts
@@ -5,12 +5,12 @@ const _http = new http();
 _http.get("https://storage.googleapis.com/audiobook_edrlab/feed.json", {}, 
   async (res) => {
 
-    console.log("test");
-    
+    console.log("request callback");
+
     res.data = await res.response?.json();
-    
+
     return res;
   }).then(async (data) => {
-  console.log(data);
+    console.log(data);
 
-});
+  });
